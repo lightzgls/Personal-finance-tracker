b84892f899b3c2114cff76aad479c2a446d83608
Sang Vu <dttmcndy2915@gmail.com>
2025-10-03 20:38:53 +0700

    feat: allow transaction deletion and source deactivation fix: add input validation when creating a transaction

--- handler/handler.go
+++ handler/handler.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/schema"
 	"github.com/jackc/pgx/v5"
 )
@@ -73,92 +74,13 @@ func AddTransactionHandler(db *pgx.Conn, tmpl *template.Template) http.HandlerFu
 		}
 		log.Printf("Form data received: %+v", r.PostForm)
 
-		var req model.AddTransactionRequest
-
-		err := decoder.Decode(&req, r.PostForm)
-		if err != nil {
-			http.Error(w, "Failed to decode form data", http.StatusBadRequest)
-			log.Printf("!!! Failed to decode form data: %v", err)
-			return
-		}
-		_, err = time.Parse("2006-01-02", req.TransactionDate)
-		if err != nil {
-			log.Printf("Invalid date format: %v", err)
-			http.Error(w, "Invalid date format. Please use dd/mm/YYYY.", http.StatusBadRequest)
-			return
-		}
-		err = repository.AddTransactions(db, req)
-		if errors.Is(err, repository.ErrNotEnoughBalance) {
-			log.Println("Insufficient balance, re-rendering page with error...")
-			transactions, err := repository.GetAllTransactions(db)
-			if err != nil {
-				http.Error(w, "Failed to fetch transactions", http.StatusInternalServerError)
-				return
-			}
-
-			balance, monthIncome, monthExpense, err := repository.GetSummary(db)
-			if err != nil {
-				http.Error(w, "Failed to fetch balance", http.StatusInternalServerError)
-				return
-			}
-
-			sources, err := repository.GetAllSoucesName(db)
-			if err != nil {
-				http.Error(w, "Failed to fetch sources", http.StatusInternalServerError)
-				return
-			}
-			limit := 5
-			if len(transactions) < limit {
-				limit = len(transactions)
-			}
-			limitedTransactions := transactions[:limit]
-
-			response := model.GetSummaryResponse{
-				Balance:          balance,
-				MonthIncome:      monthIncome,
-				MonthExpense:     monthExpense,
-				Transactions:     limitedTransactions,
-				AllTransactions:  transactions,
-				AvailableSources: sources,
-
-				FormErrors: map[string]string{
-					"not_enough_balance": "Insufficient funds in this account.",
-				},
-			}
-
-			err = tmpl.ExecuteTemplate(w, "home.html", response)
-			if err != nil {
-				log.Printf("Failed to render template: %v", err)
-			}
-			return
-		}
-
-		if err != nil {
-			http.Error(w, "An internal server error occurred", http.StatusInternalServerError)
-			return
-		}
-
-		log.Println("Transaction added successfully, redirecting.")
-		http.Redirect(w, r, "/home", http.StatusSeeOther)
-	}
-}
-
-func GetSummaryHandler(db *pgx.Conn, tmpl *template.Template) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// 1. Ensure the method is GET
-		if r.Method != http.MethodGet {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// 2. Fetch all transactions from the repository
 		transactions, err := repository.GetAllTransactions(db)
 		if err != nil {
 			http.Error(w, "Failed to fetch transactions", http.StatusInternalServerError)
 			return
 		}
 
-		// 3. Fetch summary data from the repository
+		
 		balance, monthIncome, monthExpense, err := repository.GetSummary(db)
 		if err != nil {
 			http.Error(w, "Failed to fetch balance", http.StatusInternalServerError)
@@ -170,7 +92,6 @@ func GetSummaryHandler(db *pgx.Conn, tmpl *template.Template) http.HandlerFunc {
 		}
 		limitedTransactions := transactions[:limit]
 
-		//popup for transaction history
 		var TransPopup = false
 		var sourcePopup = false
 
@@ -188,25 +109,165 @@ func GetSummaryHandler(db *pgx.Conn, tmpl *template.Template) http.HandlerFunc {
 			AllSources = sourcesWithBalance
 		}
 
-		// get sources name
 		sources, err := repository.GetAllSoucesName(db)
 		if err != nil {
 			http.Error(w, "Failed to fetch sources", http.StatusInternalServerError)
 			return
 		}
 
-		//responsible for the red text under the input box
-		formErrors := make(map[string]string)
-		errorKey := r.URL.Query().Get("error")
-		if errorKey == "source_already_exist" {
-			formErrors["source_name"] = "This source already exists. Please choose another."
-		} else if errorKey == "negative_balance" {
-			formErrors["balance"] = "Initial balance cannot be a negative number."
-		} else if errorKey == "balance_low" {
-			formErrors["not_enough_balance"] = "Insufficient funds in this account."
+
+
+
+		var req model.AddTransactionRequest
+
+		err = decoder.Decode(&req, r.PostForm)
+		if err != nil {
+			http.Error(w, "Failed to decode form data", http.StatusBadRequest)
+			log.Printf("!!! Failed to decode form data: %v", err)
+			return
 		}
 
-		response := model.GetSummaryResponse{
+
+
+		_, err = time.Parse("2006-01-02", req.TransactionDate)
+		if err != nil {
+			log.Printf("Invalid date format: %v", err)
+			http.Error(w, "Invalid date format. Please use dd/mm/YYYY.", http.StatusBadRequest)
+			return
+		}
+
+
+
+		err = repository.AddTransactions(db, req)
+		if errors.Is(err, repository.ErrNotEnoughBalance) {
+			log.Println("Insufficient balance, re-rendering page with error...")
+
+
+
+			response := model.PageData{
+				Balance:          balance,
+				MonthIncome:      monthIncome,
+				MonthExpense:     monthExpense,
+				Transactions:     limitedTransactions,
+				FormErrors:       make(map[string]string),
+				ShowTransPopup:   TransPopup,
+				AllTransactions:  transactions,
+				AvailableSources: sources,
+				ShowSourcesPopup: sourcePopup,
+				AllSources:       AllSources,
+			}
+
+
+
+
+			response.FormErrors["not_enough_balance"] = err.Error()
+			err = tmpl.ExecuteTemplate(w, "home.html", response)
+			if err != nil {
+				log.Printf("Failed to render template: %v", err)
+			}
+			return
+
+
+		} else if errors.Is(err,repository.ErrNegativeAmount) {
+			log.Println("Negative amount, re-rendering page with error...")
+
+
+			response := model.PageData{
+				Balance:          balance,
+				MonthIncome:      monthIncome,
+				MonthExpense:     monthExpense,
+				Transactions:     limitedTransactions,
+				FormErrors:       make(map[string]string),
+				ShowTransPopup:   TransPopup,
+				AllTransactions:  transactions,
+				AvailableSources: sources,
+				ShowSourcesPopup: sourcePopup,
+				AllSources:       AllSources,
+			}
+
+			response.FormErrors["negative_amount"] = err.Error()
+
+
+
+			err = tmpl.ExecuteTemplate(w, "home.html", response)
+			if err != nil {
+				log.Printf("Failed to render template: %v", err)
+			}
+
+			return
+		} else if err != nil {
+			http.Error(w, "An internal server error occurred", http.StatusInternalServerError)
+			return
+		}
+
+
+
+		log.Println("Transaction added successfully, redirecting.")
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+	}
+}
+
+func GetSummaryHandler(db *pgx.Conn, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+	
+		transactions, err := repository.GetAllTransactions(db)
+		if err != nil {
+			http.Error(w, "Failed to fetch transactions", http.StatusInternalServerError)
+			return
+		}
+
+		
+		balance, monthIncome, monthExpense, err := repository.GetSummary(db)
+		if err != nil {
+			http.Error(w, "Failed to fetch balance", http.StatusInternalServerError)
+			return
+		}
+		limit := 5
+		if len(transactions) < limit {
+			limit = len(transactions)
+		}
+		limitedTransactions := transactions[:limit]
+
+
+		var TransPopup = false
+		var sourcePopup = false
+
+		if r.URL.Query().Get("show_all_transactions") == "true" {
+			TransPopup = true
+		}
+		var AllSources []model.Account
+		if r.URL.Query().Get("show_all_sources") == "true" {
+			sourcePopup = true
+			sourcesWithBalance, err := repository.GetAllSources(db)
+			if err != nil {
+				http.Error(w, "Failed to fetch source balances", http.StatusInternalServerError)
+				return
+			}
+			AllSources = sourcesWithBalance
+		}
+
+		sources, err := repository.GetAllSoucesName(db)
+		if err != nil {
+			http.Error(w, "Failed to fetch sources", http.StatusInternalServerError)
+			return
+		}
+
+		formErrors := make(map[string]string)
+		errorKey := r.URL.Query().Get("error")
+		switch errorKey {
+			case "source_already_exist":
+				formErrors["source_name"] = "This source already exists. Please choose another."
+			case "negative_balance":
+				formErrors["balance"] = "Initial balance cannot be a negative number."
+		}
+
+		response := model.PageData{
 			Balance:          balance,
 			MonthIncome:      monthIncome,
 			MonthExpense:     monthExpense,
@@ -248,6 +309,72 @@ func GetAllSourcesHandler(db *pgx.Conn) http.HandlerFunc {
 		}
 
 	}
+}
+
+func DeleteTransactionsHandler(db *pgx.Conn) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+            return
+        }
+
+        if err := r.ParseForm(); err != nil {
+            http.Error(w, "Failed to parse form", http.StatusBadRequest)
+            return
+        }
+
+        idStrings := r.PostForm["trans_id"]
+        if len(idStrings) == 0 {
+            http.Redirect(w, r, "/home?show_all_transactions=true", http.StatusSeeOther)
+            return
+        }
+
+        var idsToDelete []uuid.UUID
+        for _, idStr := range idStrings {
+            id, err := uuid.Parse(idStr)
+            if err != nil {
+                http.Error(w, "Invalid transaction ID found", http.StatusBadRequest)
+                return
+            }
+            idsToDelete = append(idsToDelete, id)
+        }
+
+        _, err := repository.DeleteTransactionsByIDs(db, idsToDelete)
+        if err != nil {
+            log.Printf("Failed to delete transactions: %v", err)
+            http.Error(w, "Failed to delete transactions", http.StatusInternalServerError)
+            return
+        }
+
+        http.Redirect(w, r, "/home?show_all_transactions=true", http.StatusSeeOther)
+    }
+}
+func InactiveSoucesHandler(db *pgx.Conn) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != http.MethodPost {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+            return
+        }
+
+        if err := r.ParseForm(); err != nil {
+            http.Error(w, "Failed to parse form", http.StatusBadRequest)
+            return
+        }
+		log.Println("handler")
+        names := r.PostForm["source_name"]
+        if len(names) == 0 {
+            http.Redirect(w, r, "/home?show_all_sources=true", http.StatusSeeOther)
+            return
+        }
+        _, err := repository.InactiveSources(db, names)
+        if err != nil {
+            log.Printf("Failed to delete transactions: %v", err)
+            http.Error(w, "Failed to delete transactions", http.StatusInternalServerError)
+            return
+        }
+
+        http.Redirect(w, r, "/home?show_all_sources=true", http.StatusSeeOther)
+    }
 }
 
 // func GetAllSoucesNameHandler(db *pgx.Conn) http.HandlerFunc{
